c714055e1b81ef2616ded570ce449a899fbbccdf
Aria <ariahassanpour@gmail.com>
2024-06-20 11:55:29 +0330

    subjects list enhanced (heading added)

--- src/components/Subjects.tsx
+++ src/components/Subjects.tsx
@@ -1,4 +1,4 @@
-import { Button, List, ListItem, Text } from "@chakra-ui/react";
+import { Button, Heading, List, ListItem, Text } from "@chakra-ui/react";
 import { Subject, bookSubjects } from "../consts/subjects";
 interface Props {
   onSelectSubject: (subject: Subject) => void;
@@ -6,22 +6,30 @@ interface Props {
 }
 const Subjects = (props: Props) => {
   return (
-    <List>
-      {bookSubjects.map((subj) => (
-        <ListItem key={subj.id} paddingY="5px">
-          <Button
-            fontSize="lg"
-            fontWeight={
-              props.selectedSubject.id === subj.id ? "bold" : "normal"
-            }
-            variant="link"
-            onClick={() => props.onSelectSubject(subj)}
-          >
-            {subj.name}
-          </Button>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize="2xl" marginTop={4} marginBottom={2}>
+        Subjects:
+      </Heading>
+      <List>
+        {bookSubjects.map((subj) => (
+          <ListItem key={subj.id} paddingY="5px">
+            <Text
+              fontSize={props.selectedSubject.id === subj.id ? "xl" : "lg"}
+              fontWeight={
+                props.selectedSubject.id === subj.id ? "bold" : "normal"
+              }
+              variant="link"
+              whiteSpace="normal"
+              textAlign="left"
+              padding={0}
+              onClick={() => props.onSelectSubject(subj)}
+            >
+              {subj.name}
+            </Text>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
