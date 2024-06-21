35b8f9b3d4d45e3d925cfe5101eeb43edb933154
Aria <ariahassanpour@gmail.com>
2024-06-21 18:27:22 +0330

    changed baseUrl back to herokuapp to test on Vercel

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,8 +3,8 @@ export default axios.create({
     headers: {
         'Access-Control-Allow-Origin': '*'
     },
-    baseURL: 'https://openlibrary.org',
+    // baseURL: 'https://openlibrary.org',
     
-    // baseURL: 'https://cors-anywhere.herokuapp.com/https://openlibrary.org',
+    baseURL: 'https://cors-anywhere.herokuapp.com/https://openlibrary.org',
 })
 export {CanceledError}
